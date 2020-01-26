08f81080f9839605fd2468055a82e08261872148
harshil <harshilshah4251@gmail.com>
2020-01-26 17:13:42 -0500

    removed duplicates from source 1

--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
+JSON Structures:
+
+File: text/word_list.json
+Notes: Contains words list along with source information.
+JSON: 
+[
+	{
+		"source": String,
+		"words" : [Unicode format]
+	}
+]
+
+File: text/all_words.json
+Notes: Contains all words without repetitions
+JSON:
+[
+Unicode format
+]
 
--- text/make_wordlist.py
+++ text/make_wordlist.py
@@ -6,7 +6,7 @@ class WordListMaker():
     def __init__(self):
         pass
     
-    def createWordList(self, fileName):
+    def createWordList(self):
 
         # formatting raw words
         #
@@ -21,7 +21,28 @@ class WordListMaker():
         #     for word in words:
         #         f.write(word+"\n")
         
-        # word_list = json.load(open("word-list.json", "r"))
+        with open("word_list.json", "r+") as word_list_f:
+            word_list_by_src = json.load(word_list_f)
+            for i, word_src_obj in enumerate(word_list_by_src):
+                print("Word count before: {}".format(len(word_src_obj["words"])))
+                words = set(word_src_obj["words"])
+                print("Word count after: {}".format(len(words)))
+                word_list_by_src[i]["words"] = list(words)
+            with open("all_words.json", "r+") as all_words_f:
+                all_words = json.load(all_words_f)
+                new_words_arr = list(set(all_words).union(words))
+                all_words_f.seek(0)
+                json.dump(new_words_arr, all_words_f, indent=4)
+                all_words_f.truncate()
+            word_list_f.seek(0)        # <--- should reset file position to the beginning.
+            json.dump(word_list_by_src, word_list_f, indent=4)
+            word_list_f.truncate()     
+
+
+
+
+
+
         # word_list.append({"source": src, "words":words})
         # json.dump(word_list, open("word-list.json", "w"))
 
@@ -35,4 +56,4 @@ class WordListMaker():
 
 if __name__ == "__main__":
     wordListMaker = WordListMaker()
-    # wordListMaker.createWordList("b1.txt")
+    wordListMaker.createWordList()
